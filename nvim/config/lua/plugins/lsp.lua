dbfb93cf38eec2fc70e9164e381df084c66a198a
erick-alcachofa <158791341+erick-alcachofa@users.noreply.github.com>
2025-02-16 22:48:20 -0600

    Small correction
    Signed-off-by: erick-alcachofa <158791341+erick-alcachofa@users.noreply.github.com>

--- nvim/config/lua/plugins/lsp.lua
+++ nvim/config/lua/plugins/lsp.lua
@@ -260,10 +260,6 @@ return {
         lsp.nvim_workspace()
         lsp.setup()
 
-        -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-        --     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-        -- })
-
         vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = {
                 prefix = "◉",
